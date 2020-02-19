35a58d4ae6f617cb4c8e5488251b6d879737be0a
Vapizkouhai <60707840+Vapizkouhai@users.noreply.github.com>
2020-02-19 14:44:48 -0300

    Update Grafico_de_linhas_1.py

--- Grafico_de_linhas_1.py
+++ Grafico_de_linhas_1.py
@@ -5,14 +5,16 @@ with urllib.request.urlopen('http://albertocn.sytes.net/2019-2/pi/projeto/geraca
   valores = list()
   listaEnergiaDia = list()
   diasGrafico = list()
+  mes = '01'
+  meses = {'01':'Janeiro', '02':'Fevereiro', '03':'Março', '04':'Abril', '05':'Maio', '06':'Junho', '07':'Julho', '08':'Agosto', '09':'Setembro', '10':'Outubro', '11':'Novembro', '12':'Dezembro'}
   for dia in energia_ano:
     valores.append(list(dia.values()))
   valores.sort()
   for dado in valores:
-    if dado[0][5:7] == '01':
+    if dado[0][5:7] == mes:
       diasGrafico.append('Dia ' + dado[0][8:])
       listaEnergiaDia.append(dado[2])
-  print(listaEnergiaDia)
-  print(diasGrafico)
   plt.plot(diasGrafico, listaEnergiaDia)
+  plt.ylabel('Energia Gerada (Kwh)')
+  plt.suptitle('Energia Gerada por Dia no Mês de ' + meses[mes])
   plt.show()
